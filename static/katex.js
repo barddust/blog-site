b6ab37163bb1190cc239c627189924bf29510704
fingerknight <fingerknight@users.noreply.github.com>
2024-02-24 13:08:37 +0800

    deploy: fingerknight/Note@5c7e96d67f9d6878804cf99c08fde0b492657f85

--- static/katex.js
+++ static/katex.js
@@ -4,23 +4,24 @@ const macros = {
     "\\Q": "\\mathbb{Q}",
     "\\R": "\\mathbb{R}",
     "\\Z": "\\mathbb{Z}",
-    "’": "'"
+    "’": "'",
+    "\\d": "\\mathrm{d}",
 };
 document.addEventListener("DOMContentLoaded", function() {
     renderMathInElement(document.getElementById("content"), {
         strict: false,
         delimiters: [
-            {"display": true,"left": "$$","right": "$$"},
-            {"display": true,"left": "\\[","right": "\\]"},
-            {"display": true,"left": "\\begin{equation}","right": "\\end{equation}"},
-            {"display": true,"left": "\\begin{equation*}","right": "\\end{equation*}"},
-            {"display": true,"left": "\\begin{align}","right": "\\end{align}"},
-            {"display": true,"left": "\\begin{align*}","right": "\\end{align*}"},
-            {"display": true,"left": "\\begin{alignat}","right": "\\end{alignat}"},
-            {"display": true,"left": "\\begin{alignat*}","right": "\\end{alignat*}"},
-            {"display": true,"left": "\\begin{gather}","right": "\\end{gather}"},
-            {"display": true,"left": "\\begin{CD}","right": "\\end{CD}"},
-            {"display": false,"left": "$","right": "$"},
-            {"display": false,"left": "\\(","right": "\\)"}
+            {"display": true, "left": "$$", "right": "$$"},
+            {"display": true, "left": "\\[", "right": "\\]"},
+            {"display": true, "left": "\\begin{equation}",  "right": "\\end{equation}"},
+            {"display": true, "left": "\\begin{equation*}",  "right": "\\end{equation*}"},
+            {"display": true, "left": "\\begin{align}", "right": "\\end{align}"},
+            {"display": true, "left": "\\begin{align*}", "right": "\\end{align*}"},
+            {"display": true, "left": "\\begin{alignat}", "right": "\\end{alignat}"},
+            {"display": true, "left": "\\begin{alignat*}", "right": "\\end{alignat*}"},
+            {"display": true, "left": "\\begin{gather}", "right": "\\end{gather}"},
+            {"display": true, "left": "\\begin{CD}", "right": "\\end{CD}"},
+            {"display": false, "left": "$", "right": "$"},
+            {"display": false, "left": "\\(", "right": "\\)"},
         ],
         macros});})
